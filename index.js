c25af420e9c681242bd9fb4c555a54af8dd73221
jvfe <jvfecav@gmail.com>
2022-02-27 15:30:49 -0300

    feat: Create config file and add index page
    - Adds _config.yml that is read from
    - Add index file template

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
 const ejs = require("ejs");
+const config = yaml.load(fs.readFileSync("_config.yml"));
 
-const readSPARQL = (dirpath) => {
+const readSPARQL = (dirPath) => {
   let stringData = [];
 
-  let commentRe = /#\+.*/g;
+  const commentRe = /#\+.*/g;
 
-  fs.readdirSync(dirpath).forEach((file) => {
-    const fileContent = fs.readFileSync(`${dirpath}${file}`).toString();
+  fs.readdirSync(dirPath).forEach((file) => {
+    const fileContent = fs.readFileSync(`${dirPath}${file}`).toString();
     const grlcDecorators = yaml.load(
       fileContent
         .match(commentRe)
@@ -21,22 +22,35 @@ const readSPARQL = (dirpath) => {
       query: fileContent,
       encodedQuery: encodeURIComponent(fileContent),
       title: grlcDecorators["summary"],
-      layout: "query.ejs",
     });
   });
   return stringData;
 };
 
-const queriesArr = readSPARQL("./queries/");
+const renderAll = (config) => {
+  const queriesArr = readSPARQL(config["queries_directory"]);
+  const allData = {
+    queryFiles: queriesArr,
+    ...config,
+  };
 
-queriesArr.forEach((query) => {
-  ejs.renderFile("./templates/query.ejs", query, {}, (err, str) => {
+  queriesArr.forEach((query) => {
+    ejs.renderFile(allData["query_template"], query, {}, (err, str) => {
+      if (err) throw err;
+
+      fs.writeFile(`./_site/queries/${query.filename}.html`, str, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+
+  ejs.renderFile(allData["index_template"], allData, {}, (err, str) => {
     if (err) throw err;
 
-    fs.writeFile(`./_site/${query.filename}.html`, str, (err) => {
+    fs.writeFile("./_site/index.html", str, (err) => {
       if (err) throw err;
     });
   });
-});
+};
 
-module.exports = readSPARQL;
+renderAll(config);
